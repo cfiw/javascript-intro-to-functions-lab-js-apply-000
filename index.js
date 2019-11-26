adb226b1ada2d593e6090fc8163106c7011e3d38
charming-modem-2150 <carrie.wilmarth@gmail.com>
2019-11-26 16:53:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
-function logShout(string) {console.log(string.toUppercase)}
-function logWhisper(string) {console.log(string.toLowercase)}
+function logShout('hello') {console.log(string.toUppercase)}
+function logWhisper('HELLO') {console.log('hello')}
