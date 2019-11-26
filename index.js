36b41f1aa43c7b8faba602c4d07cae392afdca4c
charming-modem-2150 <carrie.wilmarth@gmail.com>
2019-11-26 16:34:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 function shout(string) {return string.toUpperCase()}
 function whisper(string) {return string.toLowerCase()}
-function sayHiToGrandma(string) { if (string.toUpperCase === uppercase) {return 'I can\'t hear you!'
+function sayHiToGrandma(string) { if (string.toUpperCase === uppercase) {return 'I can\'t hear you!'}}
